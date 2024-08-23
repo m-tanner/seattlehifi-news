6322d536be030723930e5bd33109de5655a3f15e
Tanner <57499600+m-tanner@users.noreply.github.com>
2024-08-23 09:18:41 -0700

    Add buy me a coffee button and github issue button (#7)

--- src/AuthForm.js
+++ src/AuthForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const frontEndURL = process.env.REACT_APP_FRONTEND_BASE_URL;
 
@@ -57,6 +57,16 @@ const AuthForm = () => {
                     />
                 </div>
                 <button type="submit">Submit</button>
+                <a href="https://www.buymeacoffee.com/m.tanner" target="_blank" rel="noopener noreferrer"
+                   className="coffee-button">
+                    <img src="https://cdn-icons-png.flaticon.com/512/1046/1046754.png" alt="Coffee Icon"/>
+                    Buy Me a Coffee
+                </a>
+                <a href="https://github.com/m-tanner/hawthornestereo-news/issues" target="_blank"
+                   rel="noopener noreferrer" className="github-button">
+                    <img src="https://cdn-icons-png.flaticon.com/512/733/733553.png" alt="GitHub Icon"/>
+                    Submit Issues & Feature Requests
+                </a>
                 {message && <p>{message}</p>}
             </form>
         </div>
--- src/UserProfileForm.js
+++ src/UserProfileForm.js
@@ -189,11 +189,21 @@ const UserProfileForm = () => {
             />
           </div>
           <button type="submit">Save</button>
+          <a href="https://www.buymeacoffee.com/m.tanner" target="_blank" rel="noopener noreferrer"
+             className="coffee-button">
+            <img src="https://cdn-icons-png.flaticon.com/512/1046/1046754.png" alt="Coffee Icon"/>
+            Buy Me a Coffee
+          </a>
+          <a href="https://github.com/m-tanner/hawthornestereo-news/issues" target="_blank" rel="noopener noreferrer"
+             className="github-button">
+            <img src="https://cdn-icons-png.flaticon.com/512/733/733553.png" alt="GitHub Icon"/>
+            Submit Issues & Feature Requests
+          </a>
           {error && <p>Error: {error}</p>}
         </form>
         {/* Conditionally render the "Trigger Notifications" button */}
         {(formData.email_address.endsWith('@hawthornestereo.com') || formData.email_address.endsWith('@tanner-wei.com')) && (
-            <div style={{marginTop: '20px'}}>
+            <div>
               <button onClick={triggerNotifications}>Trigger Notifications</button>
               {/* Show trigger response */}
               {triggerResponse && (
